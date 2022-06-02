775295f5e8f77a236b93c4b9485f27a99989e45f
Gee-png <gloryamuah@gmail.com>
2022-06-02 11:51:16 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
+task 5:
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
