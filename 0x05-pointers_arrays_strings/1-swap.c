129ee6855294af166ad69507cef995f8fe09892c
Gee-png <gloryamuah@gmail.com>
2022-06-13 14:25:20 -0700

    Adding 1-swap.c file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 
 /**
- * swap - swaps the values of two integers.
- * @a: first integer.
- * @b: second integer.
- * Return: no return.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void swap_int(int *a, int *b)
+int main(void)
 {
-	int temp;
+    int a;
+    int b;
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
 }
 
